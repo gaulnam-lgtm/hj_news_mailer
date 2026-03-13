b17c2bda0d892fcd723f24982e5f48cd1cae74d8
gaulnam-lgtm <gaulnam@gmail.com>
2026-03-13 16:44:38 +0900

    Update news_mailer.py

--- news_mailer.py
+++ news_mailer.py
@@ -818,7 +818,7 @@ def send_mail(html):
     recipients = [x.strip() for x in MAIL_TO.split(",") if x.strip()]
     msg = MIMEMultipart("alternative")
     msg["Subject"] = f"[앱 마켓 뉴스 레터] {today}"
-   msg["From"] = f"KISA(김형진) <{GMAIL_ID}>"
+    msg["From"] = f"KISA(김형진) <{GMAIL_ID}>"
     msg["To"] = ", ".join(recipients)
     msg.attach(MIMEText(html, "html", "utf-8"))
 
